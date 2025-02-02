acf99847dcd3bc01129827c1f7813fe38791d77c
david-gutsal <davidgutierrezsalvat@gmail.com>
2025-02-02 19:45:35 +0100

    (Update)Fin de la pagina principal

--- Lenguaje/RestOfTheRonin/assets/js/script.js
+++ Lenguaje/RestOfTheRonin/assets/js/script.js
@@ -1,9 +1,7 @@
-// Obtener referencias al div y al botón
+
 const div = document.getElementById("info");
 const boton = document.getElementById("ocultar-btn");
 
-// Agregar un evento click al botón
 boton.addEventListener("click", function () {
-  // Ocultar el div cambiando el estilo
   div.style.display = "none";
 });
